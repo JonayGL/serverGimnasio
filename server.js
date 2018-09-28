c7ae8138b5eb429b6917ee25492a4efeee8f928a
Jonay <jonaygilabert@gmail.com>
2018-09-28 18:21:40 +0200

    asdfas

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.listen(process.env.PORT || 3000, function ()  {
   //console.log(`server is listening on ${port}`)
 
 
-  
+  knex.schema.dropTableIfExists('users');
   knex.schema.createTable('users', function (table) {
     table.string('nick');
     table.integer('pass');
